4624cae6711735003e1c2e641eca787cc48c8cb9
ShaileshIshere <markvaldoz009@gmail.com>
2024-07-12 08:56:04 +0530

    solved signup/signin problem

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,8 +1,8 @@
-export const InputBox = ({ label, placeholder }) => {
+export const InputBox = ({ onChange, label, placeholder }) => {
     return <div>
         <div className="text-lg font-medium text-left py-2">
             { label }
         </div>
-        <input type="text" placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+        <input onChange={onChange} type="text" placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
     </div>
 }
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -3,11 +3,39 @@ import { Heading } from "../components/Heading"
 import { SubHeading } from "../components/SubHeading"
 import { InputBox } from "../components/InputBox"
 import { BottomWarning } from "../components/BottomWarning"
-import diffFlowers from "../images/flowers.jpg";
 import { useNavigate } from "react-router-dom"
+import axios from "axios"
+import diffFlowers from "../images/flowers.jpg"
+import { useState } from "react"
 
 export const Signin = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    
+    const handleSignin = async () => {
+        try {
+            const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+                username,
+                password
+            })
+            localStorage.setItem("token", response.data.token)
+            navigate("/dashboard")
+        } 
+        catch (error) {
+            if (error.response) {
+                // Server responded with a status other than 200 range
+                console.error("Error response", error.response.data);
+            } else if (error.request) {
+                // Request was made but no response received
+                console.error("Error request", error.request);
+            } else {
+                // Something else happened while setting up the request
+                console.error("Error message", error.message);
+            }
+        }
+    }
+
     return(
         <div className="flex w-full h-screen items-center justify-center bg-neutral-100">
             <div className="flex shadow-3xl overflow-hidden h-5/6 w-full max-w-5xl">
@@ -18,17 +46,14 @@ export const Signin = () => {
                     <div className="w-full">
                         <Heading label={"Welcome Back!"} />
                         <SubHeading label={"Enter your credentials to access your account"} />
-                        <InputBox placeholder="enter your email" label={"Email"} />
-                        <InputBox placeholder="enter your password" label={"Password"} />
+                        <InputBox onChange={(event) => {
+                            setUsername(event.target.value)
+                        }} placeholder="enter your email" label={"Email"} />
+                        <InputBox onChange={(event) => {
+                            setPassword(event.target.value)
+                        }} placeholder="enter your password" label={"Password"} />
                         <div className="pt-4">
-                            <Button onClick={async () => {
-                                const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
-                                    username,
-                                    password
-                                });
-                                localStorage.setItem("token", response.data.token)
-                                navigate("/dashboard")
-                            }} label={"Sign in"} />
+                            <Button onClick={handleSignin} label={"Sign in"} />
                         </div>
                         <BottomWarning label={"Don't have an account?"} linkText={"Sign up"} to={"/signup"} />
                     </div>
